00e036d0b9df6f2580e980a7ebfb48b8b13c7e8d
h114mx001 <21anh.nt@vinuni.edu.vn>
2024-04-21 20:58:49 +0700

    Update some gitignore and debug scripts

--- launch.sh
+++ launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 docker run \
-
-    --mount type=bind,source="$(pwd)"/challenges,target=/challenges \ 
+    --mount type=bind,source="$(pwd)"/challenges,target=/challenges     \
     --mount type=bind,source="$(pwd)"/pwnscripts,target=/home/hacker \
     --rm -it pwncollege_kernel /bin/bash
